03375453643bec87c4ab4bbd66203e66f2fb0660
2120191 <jorgeluisromeromejia4@gmail.com>
2024-05-08 08:53:44 -0500

    ejercicios.py

---
+++ ejercicios,py.py
@@ -0,0 +1,20 @@
+## ejercic
+``` python
+edad=print(int(input("ingresar edad"))
+           if edad <=17:
+           print("eres menor")
+           if edad >= 18:
+           print("eres mayor")
+
+## ejercicio dos 
+``` python
+contraseña="123456"
+print()
+contraseña=("ingresar contraseña")
+print("contraseña_contraseña")
+
+##ejercicio tres
+``` python
+
+
+
